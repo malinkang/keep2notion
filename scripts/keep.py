9e36af1eb4575fe0e7b49117d518f2f0c7f4bcaa
malinkang <malinkang@adsconflux.com>
2024-05-21 18:04:04 +0800

    fix bug

--- scripts/keep.py
+++ scripts/keep.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import argparse
+import json
 import os
 from dotenv import load_dotenv
 import pendulum
@@ -9,8 +10,8 @@ import requests
 import utils
 from config import workout_properties_type_dict
 LOGIN_API = "https://api.gotokeep.com/v1.1/users/login"
-RUN_DATA_API = "https://api.gotokeep.com/pd/v3/stats/detail?dateUnit=all&type=running&lastDate={last_date}"
-RUN_LOG_API = "https://api.gotokeep.com/pd/v3/runninglog/{run_id}"
+RUN_DATA_API = "https://api.gotokeep.com/pd/v3/stats/detail?dateUnit=all&type=all&lastDate={last_date}"
+LOG_API = "https://api.gotokeep.com/pd/v3/{type}log/{id}"
 
 keep_headers = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
@@ -42,12 +43,15 @@ def get_run_id():
         if r.ok:
             last_date = r.json()["data"]["lastTimestamp"]
             records = r.json().get("data").get("records")
-            logs = [item.get("stats") for sublist in records for item in sublist['logs']]
-            for log in logs:
-                if log.get("id")==latest_id:
-                    return results
-                else:
-                    results.append(log)
+            for record in records:
+                for log in record.get("logs"):
+                    if (log.get("type") == "stats"):
+                        results.append(log.get("stats"))
+            # for log in logs:
+            #     if log.get("id")==latest_id:
+            #         return results
+            #     else:
+            #         results.append(log)
         print(f"last date = {last_date}")
         if not last_date:
             break
@@ -72,18 +76,20 @@ def get_lastest():
 
 
 
-def get_run_data(id,name):
-    r = requests.get(RUN_LOG_API.format(run_id=id), headers=keep_headers)
+def get_run_data(log):
+    r = requests.get(LOG_API.format(type=log.get("type"),id=log.get("id")), headers=keep_headers)
     if r.ok:
-        workout = {}
+        with open("f3.json","w") as f:
+            f.write(json.dumps(r.json(),indent=4,ensure_ascii=False))
         data = r.json().get("data")
+        workout = {}
         end_time = pendulum.from_timestamp(data.get("endTime")/1000, tz="Asia/Shanghai")
-        workout["标题"] = end_time.to_datetime_string()
-        workout["名字"] = name
-        workout["Id"] = id
+        workout["标题"] = log.get("name")
+        # workout["名字"] = data.get("name")
+        workout["Id"] = data.get("id")
         workout["开始时间"] = data.get("startTime")/1000
         workout["结束时间"] = data.get("endTime")/1000
-        workout["距离"] = round(data.get("distance"))
+        workout["距离"] = round(data.get("distance",0))
         workout["运动时长"] = data.get("duration")
         workout["平均配速"] = data.get("averagePace")
         workout["消耗热量"] = data.get("calorie")
@@ -93,23 +99,28 @@ def get_run_data(id,name):
             workout["最大心率"] = heartRate.get("maxHeartRate")
         end_time = pendulum.from_timestamp(data.get("endTime")/1000, tz="Asia/Shanghai")
         cover= data.get("shareImg")
-        add_to_notion(workout,end_time,cover)
+        if cover is None:
+            cover = log.get("trackWaterMark")
+        add_to_notion(workout,end_time,log.get("icon"),cover)
 
-def add_to_notion(workout,end_time,cover):
+def add_to_notion(workout,end_time,icon,cover):
+    print(workout)
     properties = utils.get_properties(workout, workout_properties_type_dict)
     notion_helper.get_date_relation(properties,end_time)
     parent = {
         "database_id": notion_helper.workout_database_id,
         "type": "database_id",
     }
-    icon = {"type": "emoji", "emoji": "🏃🏻"}
+    icon = utils.get_icon(icon)
     #封面长图有限制
+    print(f"cover = {cover}")
     if cover and len(cover) <=2000:
         pass
     else:
-        cover = utils.upload_cover(cover)
         if cover is None:
             cover="https://images.unsplash.com/photo-1547483238-f400e65ccd56?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        else:
+            cover = utils.upload_cover(cover)
     notion_helper.create_page(
         parent=parent, properties=properties,cover=utils.get_icon(cover), icon=icon
     )
@@ -126,9 +137,4 @@ if __name__ == "__main__":
         #按照结束时间倒序排序
         logs = sorted(logs, key=lambda x: x['endTime'])
         for log in logs:
-            id = log.get("id")
-            name = log.get("name")
-            print(f"id = {id} {name}")
-            if id == latest_id:
-                break
-            get_run_data(id,name)
+            get_run_data(log)
